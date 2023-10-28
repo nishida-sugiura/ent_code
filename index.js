9099f96c3c3b06bfc364c9878548464f3c55a5b5
nishida-sugiura <108458541+nishida-sugiura@users.noreply.github.com>
2023-10-28 12:11:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var params = (new URL(document.location)).searchParams;
-var key = params.get('key');
-
-//let daytime2 = document.getElementById("textArea").value.trim();
-
 
    let today = new Date();
     console.log(today);
@@ -15,61 +10,44 @@ var key = params.get('key');
      let hun=today.getMinutes();
     let byou =today.getSeconds();
 
-    console.log("年=" + today.getFullYear());
-    console.log("月=" + (today.getMonth()+1));
-    console.log("日=" + today.getDate());
-    console.log("時=" + today.getHours());
-    console.log("分=" + today.getMinutes());
-    console.log("秒=" + today.getSeconds());
 
+$(document).ready(function () {
 
-$(function () {
-    // 送信
-    $('#form1').submit(function () {
-  
-        var s_code = $('input[name="serialcode"]').val();
-        
+    var liffId = "2001269046-RZ90vdYB";
+    initializeLiff(liffId);
+})
 
-
-        // var date = $('input[name="date"]').val();
-        // var tool = $('input[name="tool"]').val();
-        
-  //var breed = obj.filter(function(input) {
-  //return input.name.match(/breed/);});
-        
-    let msg={};
-    
-       msg = ["code：" + s_code] ;　 //トークに送信する内容
-
-        
-        
-        sendText(String(msg)); 
-      
-        return false;
-        
-    });
-});
-
-
-function openQRCodeReader() {
-    liff.scanCode()
-        .then(result => {
-            if (result.value) {
-                // QRコードのスキャンが成功した場合
-                // スキャン結果を利用して何かを行う
-                console.log("QRコードスキャン結果:", result.value);
-
-                let qr_data = result.value;
-                
-                
-                let aaa = ["qr_data:" +qr_data];
-
-                sendText(aaa);
-            }
+function initializeLiff(liffId) {
+    liff
+        .init({
+            liffId: liffId
         })
-        .catch(err => {
-            console.error(err);
+        .then(() => {
+          // start to use LIFF's api
+          initializeApp();
+        })
+        .catch((err) => {
+            console.log('LIFF Initialization failed ', err);
         });
 }
+
+function sendText(text) {
+    // sendMessages(text);
+// }
+
+// LINEトーク画面上でメッセージ送信
+// function sendMessages(text) {
+    liff.sendMessages([{
+        'type': 'text',
+        'text': text
+    }]).then(function () {  
+        liff.closeWindow();
+        
+    }).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
+
+
 
 
